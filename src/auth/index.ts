2756ca2e39550529348402d437d6f34a193b64a9
Viviane Qian <viviane@diverta.co.jp>
2023-07-18 17:46:41 +0900

    update ke jwt with auth0 jwt

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -70,3 +70,80 @@ router.get('/verify-state', async (req, env) => {
 		}
 	);
 });
+
+router.post('/update-token', async (req, env) => {
+	const body = await req.json();
+	const { auth0Token, kurocoEdgePayload } = body;
+	if (!auth0Token || !kurocoEdgePayload) {
+		return new Response(
+			JSON.stringify({
+				error: 'missing auth0Token or kurocoEdgePayload',
+			}),
+			{
+				headers: {
+					'content-type': 'application/json',
+				},
+			}
+		);
+	}
+
+	// extract auth0 payload
+	const auth0Payload = auth0Token.split('.')[1];
+	const auth0PayloadDecoded = JSON.parse(atob(auth0Payload));
+
+	// generate new kuroco edge token
+	const header = btoa(
+		JSON.stringify({
+			alg: 'RS256',
+			typ: 'JWT',
+		})
+	);
+	const payload = btoa(
+		JSON.stringify({
+			...kurocoEdgePayload,
+			auth: auth0PayloadDecoded,
+		})
+	);
+	const privateKey = env.KE_PRIVATE_KEY;
+	const encoder = new TextEncoder();
+	const dataBuffer = encoder.encode(`${header}.${payload}`);
+	const secretBuffer = Uint8Array.from(
+		atob(privateKey)
+			.split('')
+			.map((c) => c.charCodeAt(0))
+	);
+	const key = await crypto.subtle.importKey(
+		'pkcs8',
+		secretBuffer,
+		{
+			name: 'RSASSA-PKCS1-v1_5',
+			hash: {
+				name: 'SHA-256',
+			},
+		},
+		false,
+		['sign']
+	);
+	const signatureBuffer = await crypto.subtle.sign(
+		{
+			name: 'RSASSA-PKCS1-v1_5',
+		},
+		key,
+		dataBuffer
+	);
+	const signature = btoa(String.fromCharCode(...new Uint8Array(signatureBuffer)));
+	const kurocoEdgeToken = `${header}.${payload}.${signature}`;
+
+	return new Response(
+		JSON.stringify({
+			success: true,
+			kurocoEdgeToken,
+		}),
+		{
+			headers: {
+				'content-type': 'application/json',
+				//'set-cookie': 'cookie1=value1; SameSite=None; Secure'
+			},
+		}
+	);
+});
