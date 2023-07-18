613e997f5a5e8b430e28e89058598056997b88bc
Viviane Qian <viviane@diverta.co.jp>
2023-07-18 11:17:23 +0900

    generate random state

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,10 +3,17 @@ import { Router } from 'itty-router';
 export const router = Router({ base: '/api/auth' });
 
 router.get('/state', async (req) => {
-	const state = 'dummy-state';
-	return new Response(state, {
-		headers: {
-			'content-type': 'text/plain;charset=UTF-8',
-		},
-	});
+	// generate a random state string
+	const randomNumbers = crypto.getRandomValues(new Uint8Array(32));
+	const state = randomNumbers.reduce((acc, val) => acc + val.toString(16), '');
+	return new Response(
+		JSON.stringify({
+			state,
+		}),
+		{
+			headers: {
+				'content-type': 'application/json',
+			},
+		}
+	);
 });
